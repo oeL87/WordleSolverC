495bfd1583740a8474cdeaa9f631a83208dbf28d
lzhan116 <lzhan116@student.ubc.ca>
2024-09-16 22:43:27 -0700

    small refactoring updated

--- DictionaryGenerator.c
+++ DictionaryGenerator.c
@@ -4,71 +4,10 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
+#include "HashTable.h"
 
 #define MAX_WORD_LENGTH 1000
 #define MAX_WORDS 200000
-#define WORD_LENGTH 6
-#define TABLE_SIZE 10000
-
-typedef struct Node {
-    char word[WORD_LENGTH];
-    struct Node* next; 
-} Node;
-
-typedef struct HashTable {
-    Node* table[TABLE_SIZE];
-} HashTable;
-
-unsigned int hash(const char* word) {
-    unsigned int hashValue = 0;
-    for (int x = 0; word[x] != '\0'; x++) {
-        hashValue = (hashValue * 31) + word[x];
-    }
-
-    return hashValue % TABLE_SIZE;
-}
-
-bool insertWord(HashTable* map, const char* word) {
-    unsigned int index = hash(word);
-    
-    // Check if the word already exists in the chain
-    Node* current = map->table[index];
-    while (current != NULL) {
-        if (strcmp(current->word, word) == 0) {
-            return false;  // Word already exists
-        }
-        current = current->next;
-    }
-    
-    // Insert new word at the beginning of the chain
-    Node* new_node = (Node*)malloc(sizeof(Node));
-    if (new_node == NULL) {
-        printf("Error occurred, memory allocation failed");
-        return false;
-    }
-    strcpy(new_node->word, word);
-    new_node->next = map->table[index];
-    map->table[index] = new_node;
-
-    return true;
-}
-
-void initHashMap(HashTable* map) {
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        map->table[i] = NULL;  // Initialize each bucket to NULL
-    }
-}
-
-void freeHashMap(HashTable* map) {
-    for (int i = 0; i < TABLE_SIZE; i++) {
-        Node* current = map->table[i];
-        while (current != NULL) {
-            Node* temp = current;
-            current = current->next;
-            free(temp);
-        }
-    }
-}
 
 bool isValidWord(const char *word) {
     for (int x = 0; word[x] != '\0'; x++) {
--- WordleSolver.c
+++ WordleSolver.c
@@ -2,13 +2,27 @@
 #include <math.h>
 #include <stdlib.h>
 
+
 int main() {
     FILE *in_file = fopen("memory.txt", "r");
 
     if (in_file == NULL) {
         printf("Error Occurred, could not find file memory.txt in current directory");
-        exit(-1);
+        return 1;
     }
+
+    
+    char buffer[6];
+    printf("What word did you guess? ");
+    scanf("%5s", buffer);
+
+    printf("%s", buffer);
+    //Some kind of logic
+    char nextGuess[6];
+
+    printf("You should guess %s next", nextGuess);
+
+
 
 
     return 0;
